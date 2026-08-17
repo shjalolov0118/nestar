0e6f440ce68a33413b90b0968911206e58ede623
Shukurjon/LEO <jalolovshukurjon16@gmail.com>
2026-08-18 00:03:31 +0900

    feat: develop global GraphQL error handling system

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -16,9 +16,9 @@ export class MemberService {
 			const result = await this.memberModel.create(input);
 			// TODO: Authentication via TOKEN
 			return result;
-		} catch (err) {
-			console.log('Error, Service.model:', err);
-			throw new BadRequestException(err);
+		} catch (err: any) {
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
 		}
 	}
 
