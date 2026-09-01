47b629faa20d73dfc91f46d6d56930201d269ebd
Shukurjon/LEO <jalolovshukurjon16@gmail.com>
2026-09-01 23:58:14 +0900

    feat: develop createComment, updateComment and getComments GraphQL APIs

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -1,4 +1,49 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CommentService } from './comment.service';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
+import * as mongoose from 'mongoose';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { Comment as CommentDto, Comments } from '../../libs/dto/comment/comment';
 
 @Resolver()
-export class CommentResolver {}
+export class CommentResolver {
+	constructor(private readonly commentService: CommentService) {}
+
+	@UseGuards(AuthGuard)
+	@Mutation(() => CommentDto)
+	public async createComment(
+		@Args('input') input: CommentInput,
+		@AuthMember('_id') memberId: mongoose.ObjectId,
+	): Promise<CommentDto> {
+		console.log('Mutation: createComment');
+		return await this.commentService.createComment(memberId, input);
+	}
+
+	@UseGuards(AuthGuard)
+	@Mutation(() => CommentDto)
+	public async updateComment(
+		@Args('input') input: CommentUpdate,
+		@AuthMember('_id') memberId: mongoose.ObjectId,
+	): Promise<CommentDto> {
+		console.log('Mutation: updateComment');
+		input._id = shapeIntoMongoObjectId(input._id);
+		return await this.commentService.updateComment(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query(() => Comments)
+	public async getComments(
+		@Args('input') input: CommentsInquiry,
+		@AuthMember('_id') memberId: mongoose.ObjectId,
+	): Promise<Comments> {
+		console.log('Query: getComments');
+		input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
+		const result = await this.commentService.getComments(memberId, input);
+		return result;
+	}
+}
