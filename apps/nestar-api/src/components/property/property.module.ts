46ded81f9311cb696bf2700c47094299861b19e4
Shukurjon/LEO <jalolovshukurjon16@gmail.com>
2026-09-05 00:12:39 +0900

    feat: create follow related resolver and service model

--- apps/nestar-api/src/components/property/property.module.ts
+++ apps/nestar-api/src/components/property/property.module.ts
@@ -6,7 +6,6 @@ import PropertySchema from '../../schemas/Property.model';
 import { AuthModule } from '../auth/auth.module';
 import { ViewModule } from '../view/view.module';
 import { MemberModule } from '../member/member.module';
-import { Like } from '../../libs/dto/like/like';
 import { LikeModule } from '../like/like.module';
 
 @Module({
--- apps/nestar-batch/test/app.e2e-spec.ts
+++ apps/nestar-batch/test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { NestarBatchModule } from './../src/nestar-batch.module';
 
 describe('NestarBatchController (e2e)', () => {
